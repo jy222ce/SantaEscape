6b0630b36e1544470a70d41b4c08e53347c61ada
김유림 <sleepig1@gmail.com>
2019-12-02 20:07:06 +0900

    JS

--- SantaEscape.js
+++ SantaEscape.js
@@ -186,14 +186,13 @@ Item.member('isHanded', function () {
 
 
 
-
+//시작
 
 
 
 Minigame = new Room('Minigame', '밤하늘.png')
 Town = new Room('Town', '마을.png')
 Livingroom = new Room('Livingroom', '거실.png')
-kid_room = new Room('kid_room', 'baby_background.jpg') // 아이방
 parent_room = new Room('parent_room', 'baby_background.jpg')  // 부모님방 - 배경파일 수정할 것
 
 
@@ -203,22 +202,12 @@ Minigame.santa = new Object(Minigame, 'santa', '산타썰매.png')
 Minigame.santa.resize(300)  
 Minigame.santa.locate(250, 150) 
 
-Minigame.door1 = new Door(Minigame, 'door1', '버튼.png', '버튼.png', Town)
+Minigame.door1 = new Object(Minigame, 'door1', '버튼.png')
 Minigame.door1.resize(150)
 Minigame.door1.locate(650, 620)
-Minigame.door1.close()
 Minigame.door1.onClick = function() {
-	if (!this.id.isLocked() && this.id.isClosed()){
-		this.id.open()
-	}
-	else if (this.id.isOpened()){
-		if (this.connectedTo !== undefined){
-			Game.move(this.connectedTo)
-		}
-		else {
-			Game.end()
-		}
-	}
+	
+    Game.move(Town)
 
 }
 
@@ -226,185 +215,185 @@ Minigame.door1.onClick = function() {
 // 현재는 화살표 누르면 바로 이동
 
 
-/*     마을에서 집 고르기     */
-Town.house1 = new Object(Town, 'house1', '집_1.png')
-Town.house1.resize(150)
-Town.house1.locate(500, 400)
-Town.house1.onClick = function() {
-	 playSound("황당.wav")
-	 Game.over("빈집털이범으로 오해받았다...\n 그레이스의 집은 어디?")
-}
+// /*     마을에서 집 고르기     */
+// Town.house1 = new Object(Town, 'house1', '집_1.png')
+// Town.house1.resize(150)
+// Town.house1.locate(500, 400)
+// Town.house1.onClick = function() {
+// 	 playSound("황당.wav")
+// 	 Game.over("빈집털이범으로 오해받았다...\n 그레이스의 집은 어디?")
+// }
 
-Town.house2 = new Object(Town, 'house2', '집_2.png')
-Town.house2.resize(150)
-Town.house2.locate(500, 570)
-Town.house2.onClick = function() {
-	playSound("황당.wav")
-	Game.over("여긴 티미의 집이었다...\n 그레이스의 집은 어디?")
-}
+// Town.house2 = new Object(Town, 'house2', '집_2.png')
+// Town.house2.resize(150)
+// Town.house2.locate(500, 570)
+// Town.house2.onClick = function() {
+// 	playSound("황당.wav")
+// 	Game.over("여긴 티미의 집이었다...\n 그레이스의 집은 어디?")
+// }
 
-Town.house4 = new Object(Town, 'house4', '집_4.png')
-Town.house4.resize(150)
-Town.house4.locate(700, 420)
-Town.house4.onClick = function() {
-	playSound("아모르파티.wav")
-	Game.over("밤샘파티가 벌어지는 집이었다...\n 하마터면 같이 놀 뻔")
-}
+// Town.house4 = new Object(Town, 'house4', '집_4.png')
+// Town.house4.resize(150)
+// Town.house4.locate(700, 420)
+// Town.house4.onClick = function() {
+// 	playSound("아모르파티.wav")
+// 	Game.over("밤샘파티가 벌어지는 집이었다...\n 하마터면 같이 놀 뻔")
+// }
 
-Town.door1 = new Door(Town, 'door1', '집_3.png', '집_3.png', Livingroom)
-Town.door1.resize(150)
-Town.door1.locate(700, 560)   
-Town.door1.onClick = function() {
-	if (!this.id.isLocked() && this.id.isClosed()) {
-		this.id.open()
-	} 
-	else if (this.id.isOpened()) {
-		if (this.connectedTo !== undefined) {
-			Game.move(this.connectedTo)
-			printMessage("무사히 그레이스의 집에 도착했다!") 
-		} 
-		else {
-			Game.end()
-		} 
-	}
-}
-
-
-
-/*     그레이스의 집      */
-Livingroom.setRoomLight(0.8) // 방 밝기 
-
-Livingroom.door1 = new Door(Livingroom, 'door1', '닫힌문.png', '열린문.png', kid_room)
-Livingroom.door1.resize(250)
-Livingroom.door1.locate(180, 160)
-
-Livingroom.door2 = new Door(Livingroom, 'door2', '닫힌문.png', '열린문.png', parent_room)
-Livingroom.door2.resize(250)
-Livingroom.door2.locate(1100, 160)
-Livingroom.door2.lock()
-
-Livingroom.door2.onClick = function() { // door를 클릭했을 때
-	if (Livingroom.key.isHanded() && this.id.isClosed()) { 
-		this.id.open() // key를 사용해서 door의 상태를 open으로 바꿈
-	} 
-	else if (this.id.isOpened()) { // door가 opened 상태이면
-        		game.move(parent_room) // 부모님방으로 이동
-	}
-	else if (this.id.isLocked()) { // door가 locked 상태이면
-        		printMessage("잠겨있다") // 메시지 출력
-	}
-}
+// Town.door1 = new Door(Town, 'door1', '집_3.png', '집_3.png', Livingroom)
+// Town.door1.resize(150)
+// Town.door1.locate(700, 560)   
+// Town.door1.onClick = function() {
+// 	if (!this.id.isLocked() && this.id.isClosed()) {
+// 		this.id.open()
+// 	} 
+// 	else if (this.id.isOpened()) {
+// 		if (this.connectedTo !== undefined) {
+// 			Game.move(this.connectedTo)
+// 			printMessage("무사히 그레이스의 집에 도착했다!") 
+// 		} 
+// 		else {
+// 			Game.end()
+// 		} 
+// 	}
+// }
 
 
 
-/*   열쇠   */
-Livingroom.key = new Object(Livingroom, 'key', '열쇠.png')
-Livingroom.key.resize(100)
-Livingroom.key.locate(500, 350)
-Livingroom.key.hide() // 숨김
+// /*     그레이스의 집      */
+// Livingroom.setRoomLight(0.8) // 방 밝기 
+
+// Livingroom.door1 = new Door(Livingroom, 'door1', '닫힌문.png', '열린문.png', kid_room)
+// Livingroom.door1.resize(250)
+// Livingroom.door1.locate(180, 160)
+
+// Livingroom.door2 = new Door(Livingroom, 'door2', '닫힌문.png', '열린문.png', parent_room)
+// Livingroom.door2.resize(250)
+// Livingroom.door2.locate(1100, 160)
+// Livingroom.door2.lock()
+
+// Livingroom.door2.onClick = function() { // door를 클릭했을 때
+// 	if (Livingroom.key.isHanded() && this.id.isClosed()) { 
+// 		this.id.open() // key를 사용해서 door의 상태를 open으로 바꿈
+// 	} 
+// 	else if (this.id.isOpened()) { // door가 opened 상태이면
+//         		game.move(parent_room) // 부모님방으로 이동
+// 	}
+// 	else if (this.id.isLocked()) { // door가 locked 상태이면
+//         		printMessage("잠겨있다") // 메시지 출력
+// 	}
+// }
 
 
 
-/*     벽난로     */
-Livingroom.fireplace = new Object(Livingroom, 'fireplace', '벽난로_켜짐.png')
-Livingroom.fireplace.resize(270)
-Livingroom.fireplace.locate(550, 280)
+// /*   열쇠   */
+// Livingroom.key = new Object(Livingroom, 'key', '열쇠.png')
+// Livingroom.key.resize(100)
+// Livingroom.key.locate(500, 350)
+// Livingroom.key.hide() // 숨김
 
 
 
-/*      소파      */
-Livingroom.sofa = new Object(Livingroom, 'sofa', '소파.png')
-Livingroom.sofa.resize(500)
-
-Livingroom.sofa.locate(400, 430)
-Livingroom.sofa.onClick = function () {
-    printMessage("안락한 느낌의 소파다")
-}
-
-// 드래그 모션 direction - Up, Down, Left, Right
-
-//Livingroom.sofa.onDrag = function (direction) {
-//    if (direction == "Right" && Livingroom.sofa.move) {
-//	printMessage("소파에 뭔가 있다")
-//	Livingroom.sofa.moveX(100)
-//	Livingroom.sofa.moveY(-40)
-//	Livingroom.sofa.move = false // 이후에는 더 이상 움직이지 않도록 함
-//	Livingroom.key.show()  // 열쇠 발견
-//    }
-//}
-
-//Livingroom.key.onClick = function () {
-//    printMessage("열쇠를 얻었다")
-//    Livingroom.key.pick()
-//}
-
-// 소파를 오른쪽으로 밀어서 바닥에 있는 열쇠를 발견하고, 그걸로 부모님 방을 여는 설정을 넣고 싶었는데
-// 동작이 안 되네요.....
+// /*     벽난로     */
+// Livingroom.fireplace = new Object(Livingroom, 'fireplace', '벽난로_켜짐.png')
+// Livingroom.fireplace.resize(270)
+// Livingroom.fireplace.locate(550, 280)
 
 
-/*     트리 장식     */
-Livingroom.tool1 = new Object(Livingroom, 'tool1', '장식1.png')
-Livingroom.tool1.resize(100)
-Livingroom.tool1.locate(350, 600)
 
-Livingroom.tool2 = new Object(Livingroom, 'tool2', '장식2.png')
-Livingroom.tool2.resize(100)
-Livingroom.tool2.locate(450, 620)
+// /*      소파      */
+// Livingroom.sofa = new Object(Livingroom, 'sofa', '소파.png')
+// Livingroom.sofa.resize(500)
 
-Livingroom.tool3 = new Object(Livingroom, 'tool3', '장식3.png')
-Livingroom.tool3.resize(100)
-Livingroom.tool3.locate(750, 600)
+// Livingroom.sofa.locate(400, 430)
+// Livingroom.sofa.onClick = function () {
+//     printMessage("안락한 느낌의 소파다")
+// }
 
-Livingroom.tool4 = new Object(Livingroom, 'tool4', '장식4.png')
-Livingroom.tool4.resize(100)
-Livingroom.tool4.locate(850, 600)
+// // 드래그 모션 direction - Up, Down, Left, Right
 
-Livingroom.tool5 = new Object(Livingroom, 'tool5', '장식5.png')
-Livingroom.tool5.resize(70)
-Livingroom.tool5.locate(950, 550)
+// //Livingroom.sofa.onDrag = function (direction) {
+// //    if (direction == "Right" && Livingroom.sofa.move) {
+// //	printMessage("소파에 뭔가 있다")
+// //	Livingroom.sofa.moveX(100)
+// //	Livingroom.sofa.moveY(-40)
+// //	Livingroom.sofa.move = false // 이후에는 더 이상 움직이지 않도록 함
+// //	Livingroom.key.show()  // 열쇠 발견
+// //    }
+// //}
 
-Livingroom.tool6 = new Object(Livingroom, 'tool6', '장식6.png')
-Livingroom.tool6.resize(100)
-Livingroom.tool6.locate(1050, 500)
+// //Livingroom.key.onClick = function () {
+// //    printMessage("열쇠를 얻었다")
+// //    Livingroom.key.pick()
+// //}
 
-Livingroom.tool7 = new Object(Livingroom, 'tool7', '장식7.png')
-Livingroom.tool7.resize(100)
-Livingroom.tool7.locate(550, 600)
-
-Livingroom.tool8 = new Object(Livingroom, 'tool8', '장식8.png')
-Livingroom.tool8.resize(100)
-Livingroom.tool8.locate(1050, 600)
+// // 소파를 오른쪽으로 밀어서 바닥에 있는 열쇠를 발견하고, 그걸로 부모님 방을 여는 설정을 넣고 싶었는데
+// // 동작이 안 되네요.....
 
 
-/*     트리      */
-Livingroom.tree = new Object(Livingroom, 'tree', '트리.png')
-Livingroom.tree.resize(350)
-Livingroom.tree.locate(800, 300)
+// /*     트리 장식     */
+// Livingroom.tool1 = new Object(Livingroom, 'tool1', '장식1.png')
+// Livingroom.tool1.resize(100)
+// Livingroom.tool1.locate(350, 600)
+
+// Livingroom.tool2 = new Object(Livingroom, 'tool2', '장식2.png')
+// Livingroom.tool2.resize(100)
+// Livingroom.tool2.locate(450, 620)
+
+// Livingroom.tool3 = new Object(Livingroom, 'tool3', '장식3.png')
+// Livingroom.tool3.resize(100)
+// Livingroom.tool3.locate(750, 600)
+
+// Livingroom.tool4 = new Object(Livingroom, 'tool4', '장식4.png')
+// Livingroom.tool4.resize(100)
+// Livingroom.tool4.locate(850, 600)
+
+// Livingroom.tool5 = new Object(Livingroom, 'tool5', '장식5.png')
+// Livingroom.tool5.resize(70)
+// Livingroom.tool5.locate(950, 550)
+
+// Livingroom.tool6 = new Object(Livingroom, 'tool6', '장식6.png')
+// Livingroom.tool6.resize(100)
+// Livingroom.tool6.locate(1050, 500)
+
+// Livingroom.tool7 = new Object(Livingroom, 'tool7', '장식7.png')
+// Livingroom.tool7.resize(100)
+// Livingroom.tool7.locate(550, 600)
+
+// Livingroom.tool8 = new Object(Livingroom, 'tool8', '장식8.png')
+// Livingroom.tool8.resize(100)
+// Livingroom.tool8.locate(1050, 600)
 
 
-/*     풀     */
-Livingroom.grass = new Item(Livingroom, 'grass', 'grass.png')
-Livingroom.grass.resize(50)
-Livingroom.grass.locate(800, 280)
-Livingroom.grass.setDescription("루돌프들이 좋아하는 풀이다.")
-Livingroom.grass.onClick = function () {
-    printMessage("이 풀은 뭐지?")
-    Livingroom.grass.pick()
-}
-
-// 트리 장식들을 트리로 드래그해서 꾸밀 수 있는 기능 필요
+// /*     트리      */
+// Livingroom.tree = new Object(Livingroom, 'tree', '트리.png')
+// Livingroom.tree.resize(350)
+// Livingroom.tree.locate(800, 300)
 
 
-/*    트리 그림    */
-kid_room.treepicture = new Item(kid_room, 'treepicture', '트리그림.png')
-kid_room.treepicture.resize(50)
-kid_room.treepicture.locate(800, 280)
-kid_room.treepicture.setDescription("예쁜 트리가 그려져 있는 그림. 물감이 쏟아졌던 흔적이 있다.")
+// /*     풀     */
+// Livingroom.grass = new Item(Livingroom, 'grass', 'grass.png')
+// Livingroom.grass.resize(50)
+// Livingroom.grass.locate(800, 280)
+// Livingroom.grass.setDescription("루돌프들이 좋아하는 풀이다.")
+// Livingroom.grass.onClick = function () {
+//     printMessage("이 풀은 뭐지?")
+//     Livingroom.grass.pick()
+// }
 
-kid_room.treepicture.onClick = function () {
-    printMessage("그림을 발견했다!")
-    kid_room.treepicture.pick()
-}
+// // 트리 장식들을 트리로 드래그해서 꾸밀 수 있는 기능 필요
+
+
+// /*    트리 그림    */
+// kid_room.treepicture = new Item(kid_room, 'treepicture', '트리그림.png')
+// kid_room.treepicture.resize(50)
+// kid_room.treepicture.locate(800, 280)
+// kid_room.treepicture.setDescription("예쁜 트리가 그려져 있는 그림. 물감이 쏟아졌던 흔적이 있다.")
+
+// kid_room.treepicture.onClick = function () {
+//     printMessage("그림을 발견했다!")
+//     kid_room.treepicture.pick()
+// }
 // 아이 방의 특정 공간에 숨겨져 있으면 좋겠음....
 
 
@@ -624,4 +613,4 @@ outside.rudolph2.onClick = function () {
 
 
 
-Game.start(kid_room, '방탈출에 오신 것을 환영합니다!')
+Game.start(Minigame, '방탈출에 오신 것을 환영합니다!')
